741f51f159c316189a4f726d4bd1026384d82edc
harleauxcarrera <harleauxcarrera@gmail.com>
2018-07-10 21:40:47 -0600

    registe/login react finished

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,29 @@
 import React, { Component } from "react";
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import Navbar from './comps/layout/Navbar';
+import Footer from './comps/layout/Footer';
+import Landing from './comps/layout/Landing';
+import Register from './comps/auth/Register';
+import Login from './comps/auth/Login';
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
+      <Router>
+
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <Navbar/>
+        <Route exact path = '/' component={Landing}/>
+        <div className="conatiner">
+          <Route exact path='/register' component={Register} />
+          <Route exact path='/login' component={Login} />
+          <Footer/>
+        </div>
       </div>
-    );
+
+      </Router>
+    )
   }
 }
 
