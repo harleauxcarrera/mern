473f8ed33003b6eb62f366791a9470b78f892f3e
harleauxcarrera <harleauxcarrera@gmail.com>
2018-08-01 17:27:44 -0600

    create profile working, redux state still lost on page refresh

--- client/src/comps/create-profile/CreateProfile.js
+++ client/src/comps/create-profile/CreateProfile.js
@@ -5,6 +5,9 @@ import TextFieldGroup from "../common/TextFieldGroup";
 import TextAreaFieldGroup from "../common/TextAreaFieldGroup";
 import InputGroup from "../common/InputGroup";
 import SelectListGroup from "../common/SelectListGroup";
+import { createProfile } from "../../actions/profileActions";
+import { withRouter } from "react-router-dom";
+
 class CreateProfile extends Component {
   constructor(props) {
     super(props);
@@ -31,9 +34,30 @@ class CreateProfile extends Component {
     this.onSubmit = this.onSubmit.bind(this);
   }
 
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.errors) {
+      this.setState({ errors: nextProps.errors });
+    }
+  }
+
   onSubmit(e) {
     e.preventDefault();
-    console.log("submit");
+    const profileData = {
+      handle: this.state.handle,
+      company: this.state.company,
+      website: this.state.website,
+      location: this.state.location,
+      status: this.state.status,
+      skills: this.state.skills,
+      githubusername: this.state.githubusername,
+      bio: this.state.bio,
+      twitter: this.state.twitter,
+      facebook: this.state.facebook,
+      linkedin: this.state.linkedin,
+      instagram: this.state.instagram
+    };
+
+    this.props.createProfile(profileData, this.props.history);
   }
 
   onChange(e) {
@@ -106,89 +130,96 @@ class CreateProfile extends Component {
                 Lets get some more info to make your profile stand out
               </p>
               <small className="d-block pb-3">* = required fields</small>
-              <form onSubmit={this.onSubmit} />
-              <TextFieldGroup
-                placeholder="Profile handle"
-                name="handle"
-                value={this.state.handle}
-                onChange={this.onChange}
-                error={errors.handle}
-                info=" A unique handle for your profile URL. Your full name, company name, nickname"
-              />
+              <form onSubmit={this.onSubmit}>
+                <TextFieldGroup
+                  placeholder="Profile handle"
+                  name="handle"
+                  value={this.state.handle}
+                  onChange={this.onChange}
+                  error={errors.handle}
+                  info=" A unique handle for your profile URL. Your full name, company name, nickname"
+                />
 
-              <SelectListGroup
-                placeholder="Status"
-                name="status"
-                value={this.state.status}
-                onChange={this.onChange}
-                error={errors.status}
-                info=" What kind of Dev are you?"
-                options={options}
-              />
+                <SelectListGroup
+                  placeholder="Status"
+                  name="status"
+                  value={this.state.status}
+                  onChange={this.onChange}
+                  error={errors.status}
+                  info=" What kind of Dev are you?"
+                  options={options}
+                />
 
-              <TextFieldGroup
-                placeholder="Company"
-                name="company"
-                value={this.state.company}
-                onChange={this.onChange}
-                error={errors.company}
-                info=" Where do you work?"
-              />
-              <TextFieldGroup
-                placeholder="Website"
-                name="website"
-                value={this.state.website}
-                onChange={this.onChange}
-                error={errors.website}
-                info="Personal or Company website "
-              />
-              <TextFieldGroup
-                placeholder="Location"
-                name="location"
-                value={this.state.location}
-                onChange={this.onChange}
-                error={errors.location}
-                info=" City or city & state suggested (eg. El Paso, TX)"
-              />
-              <TextFieldGroup
-                placeholder="Skills"
-                name="skills"
-                value={this.state.skills}
-                onChange={this.onChange}
-                error={errors.skills}
-                info=" Use commas to seperate values (eg. HTML, CSS, JavsScript)"
-              />
-              <TextFieldGroup
-                placeholder="Github Username"
-                name="githubusername"
-                value={this.state.handle}
-                onChange={this.onChange}
-                error={errors.handle}
-                info=" If you want your latest repos and a github link, include your username"
-              />
-              <TextAreaFieldGroup
-                placeholder="Short Bio"
-                name="bio"
-                value={this.state.bio}
-                onChange={this.onChange}
-                error={errors.bio}
-                info=" Tell us a little about yourself"
-              />
+                <TextFieldGroup
+                  placeholder="Company"
+                  name="company"
+                  value={this.state.company}
+                  onChange={this.onChange}
+                  error={errors.company}
+                  info=" Where do you work?"
+                />
+                <TextFieldGroup
+                  placeholder="Website"
+                  name="website"
+                  value={this.state.website}
+                  onChange={this.onChange}
+                  error={errors.website}
+                  info="Personal or Company website "
+                />
+                <TextFieldGroup
+                  placeholder="Location"
+                  name="location"
+                  value={this.state.location}
+                  onChange={this.onChange}
+                  error={errors.location}
+                  info=" City or city & state suggested (eg. El Paso, TX)"
+                />
+                <TextFieldGroup
+                  placeholder="Skills"
+                  name="skills"
+                  value={this.state.skills}
+                  onChange={this.onChange}
+                  error={errors.skills}
+                  info=" Use commas to seperate values (eg. HTML, CSS, JavsScript)"
+                />
+                <TextFieldGroup
+                  placeholder="Github Username"
+                  name="githubusername"
+                  value={this.state.githubusername}
+                  onChange={this.onChange}
+                  error={errors.githubusername}
+                  info=" If you want your latest repos and a github link, include your username"
+                />
+                <TextAreaFieldGroup
+                  placeholder="Short Bio"
+                  name="bio"
+                  value={this.state.bio}
+                  onChange={this.onChange}
+                  error={errors.bio}
+                  info=" Tell us a little about yourself"
+                />
 
-              <div className="mb-3">
-                <button
-                  onClick={() => {
-                    this.setState(prevState => ({
-                      displaySocialInputs: !prevState.displaySocialInputs
-                    }));
-                  }}
-                  className="btn btn-light"
-                >
-                  Add Social Network Links
-                </button>
-                <span className="text-muted"> Optional</span>
-              </div>
-              {socialInputs}
+                <div className="mb-3">
+                  <button
+                    type="button"
+                    onClick={() => {
+                      this.setState(prevState => ({
+                        displaySocialInputs: !prevState.displaySocialInputs
+                      }));
+                    }}
+                    className="btn btn-light"
+                  >
+                    Add Social Network Links
+                  </button>
+                  <span className="text-muted"> Optional</span>
+                </div>
+                {socialInputs}
+                <input
+                  type="submit"
+                  value="Submit"
+                  className="btn btn-info btn-block mt-4"
+                />
+              </form>
             </div>
           </div>
         </div>
@@ -207,4 +238,7 @@ const mapStateToProps = state => ({
   errors: state.errors
 });
 
-export default connect(mapStateToProps)(CreateProfile);
+export default connect(
+  mapStateToProps,
+  { createProfile }
+)(withRouter(CreateProfile));
