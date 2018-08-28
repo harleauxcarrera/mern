71c10f2ea74f09e280bedf27d07e3783190a2a8a
harleauxcarrera <harleauxcarrera@gmail.com>
2018-08-27 18:02:19 -0600

    fix ProfileCreds render error

--- client/src/comps/profile/Profile.js
+++ client/src/comps/profile/Profile.js
@@ -9,7 +9,6 @@ import ProfileGithub from "./ProfileGithub";
 import ProfileCreds from "./ProfileCreds";
 import Spinner from "../common/Spinner";
 import { getProfileByHandle } from "../../actions/profileActions";
-import isEmpty from "../../validation/is-empty";
 
 class Profile extends Component {
   componentDidMount() {
@@ -25,83 +24,19 @@ class Profile extends Component {
       profileContent = <Spinner />;
     } else {
       profileContent = (
-        <div className="row">
-          <div className="col-md-12">
-            <div className="card card-body bg-info text-white mb-3">
-              <div className="row">
-                <div className="col-4 col-md-3 m-auto">
-                  <img
-                    className="rounded-circle"
-                    src={profile.user.avatar}
-                    alt=""
-                  />
-                </div>
-              </div>
-              <div className="text-center">
-                <h1 className="display-4 text-center">{profile.user.name}</h1>
-                <p className="lead text-center">
-                  {profile.status}
-                  {isEmpty(profile.company) ? null : (
-                    <span> at {profile.company}</span>
-                  )}
-                </p>
-
-                {isEmpty(profile.location) ? null : <p> {profile.location}</p>}
-
-                <p>
-                  {isEmpty(profile.website) ? null : (
-                    <a
-                      className="text-white p-2"
-                      href={`https://` + profile.website}
-                      target="_blank"
-                    >
-                      <i className="fas fa-globe fa-2x" />
-                    </a>
-                  )}
-
-                  {isEmpty(profile.social && profile.social.twitter) ? null : (
-                    <a
-                      className="text-white p-2"
-                      href={`https://` + profile.social.twitter}
-                      target="_blank"
-                    >
-                      <i className="fab fa-twitter fa-2x" />
-                    </a>
-                  )}
-
-                  {isEmpty(profile.social && profile.social.facebook) ? null : (
-                    <a
-                      className="text-white p-2"
-                      href={`https://` + profile.social.facebook}
-                      target="_blank"
-                    >
-                      <i className="fab fa-facebook fa-2x" />
-                    </a>
-                  )}
-                  {isEmpty(profile.social && profile.social.linkedin) ? null : (
-                    <a
-                      className="text-white p-2"
-                      href={`https://` + profile.social.linkedin}
-                      target="_blank"
-                    >
-                      <i className="fab fa-linkedin fa-2x" />
-                    </a>
-                  )}
-                  {isEmpty(
-                    profile.social && profile.social.instagram
-                  ) ? null : (
-                    <a
-                      className="text-white p-2"
-                      href={`https://` + profile.social.instagram}
-                      target="_blank"
-                    >
-                      <i className="fab fa-instagram fa-2x" />
-                    </a>
-                  )}
-                </p>
-              </div>
+        <div>
+          <div className="row">
+            <div className="col-md-6">
+              <Link to="/profiles" className="btn btn-light mb-3 float-left">
+                Back To Profiles
+              </Link>
             </div>
+            <div className="col-md-6" />
           </div>
+          <ProfileHeader profile={profile} />
+          <ProfileAbout profile={profile} />
+
+          <ProfileGithub />
         </div>
       );
     }
